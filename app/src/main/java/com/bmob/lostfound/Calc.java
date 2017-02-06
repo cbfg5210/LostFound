b83a57f6b2b8377fe4d38027d3e53ff9da499d5c
codinger15614 <120276969@qq.com>
2017-02-06 20:56:22 +0800

    hello

---
+++ app/src/main/java/com/bmob/lostfound/Calc.java
@@ -0,0 +1,14 @@
+package com.bmob.lostfound;
+
+/**
+ * Created by hawk on 2017/2/6.
+ */
+
+public class Calc {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
